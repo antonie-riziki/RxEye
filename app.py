c4e713124bcd0dc61a76017af86e47654779d27d
antonie <53366054+antonie-riziki@users.noreply.github.com>
2025-07-03 09:55:49 +0000

    changes to the app file

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st 
+from st_social_media_links import SocialMediaIcons
+from streamlit.components.v1 import html
 
 
 
@@ -9,6 +11,41 @@ home_page = st.Page("./pgs/main.py", title="home page", icon=":material/home:")
 chatbot_page = st.Page("./pgs/chatbot.py", title="chatbot", icon=":material/chat:")
 
 
+
+
+
+
+
+with st.sidebar:
+    button = """
+        <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="echominds" data-color="#FFDD00" data-emoji=""  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#000000" data-coffee-color="#ffffff" ></script>
+        """
+
+    html(button, height=70, width=220)
+    st.markdown(
+        """
+        <style>
+            iframe[width="220"] {
+                position: fixed;
+                bottom: 60px;
+                right: 40px;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+
+    social_media_links = [
+        "https://www.x.com/am_tonie",
+        "https://www.youtube.com/@echobytes-ke",
+        "https://www.instagram.com/antonie_generall",
+        "https://www.github.com/antonie-riziki",
+    ]
+
+    social_media_icons = SocialMediaIcons(social_media_links)
+
+    social_media_icons.render()
 
 pg = st.navigation([reg_page, signin_page, dashboard_page, home_page, chatbot_page])
 
@@ -31,6 +68,10 @@ st.set_page_config(
         """
     }
 )
+
+
+
+
 
 pg.run()
 
